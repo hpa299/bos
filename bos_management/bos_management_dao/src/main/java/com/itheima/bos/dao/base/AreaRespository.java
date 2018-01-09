f678b05d0047ac0e21dddda01d66d3af4cc62412
hpa <hpa@qq.com>
2018-01-09 12:22:25 +0800

    初始化3

--- bos_management/bos_management_dao/src/main/java/com/itheima/bos/dao/base/AreaRespository.java
+++ bos_management/bos_management_dao/src/main/java/com/itheima/bos/dao/base/AreaRespository.java
@@ -12,5 +12,4 @@ public interface AreaRespository extends JpaRepository<Area,Long> {
     // 根据用户在输入框中输入的内容,模糊匹配区域
     @Query("from Area where province like ?1 or city like ?1 or  district like ?1 or postcode like ?1 or citycode like ?1 or shortcode like ?1")
     List<Area> findByQ(String q);
-
 }
